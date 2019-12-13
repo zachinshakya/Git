238d595b76824307e191fafaf5d102d4c4ce4d40
zachinshakya <sachinshakya507@gmail.com>
2019-12-13 11:00:31 +0545

    message

--- app/src/main/java/com/example/git/MainActivity.kt
+++ app/src/main/java/com/example/git/MainActivity.kt
@@ -7,6 +7,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        //sachin//
         setContentView(R.layout.activity_main)
     }
 }
